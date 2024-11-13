5cc8a75f1be772ffd568e70e1cc660d723702a0e
Paula Vartic <paula.vartic@gmail.com>
2024-11-13 23:40:17 +0000

    initial

---
+++ users/models.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        verbose_name='Email',
+        help_text='Your email',
+        unique=True
+    )
+    phone = models.CharField(
+        verbose_name='Phone number',
+        help_text='Your phone number',
+        max_length=35,
+        blank=True,
+        null=True
+    )
+    town = models.CharField(
+        verbose_name='Town',
+        help_text='Where are you from?',
+        max_length=40,
+        blank=True,
+        null=True
+    )
+    avatar = models.ImageField(
+        verbose_name='Photo',
+        help_text='Your photo',
+        upload_to='users/avatars',
+        blank=True,
+        null=True
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+
+
